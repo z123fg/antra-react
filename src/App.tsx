f5b47103b9890a11e7f6102789f3bef12b976191
Lee Sing Chan Yau <jukerhdchan@gmail.com>
2023-06-25 23:28:32 -0700

    Core functionality working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MyButton from "./components/Button/Button"
+import MyButton from './components/Button/Button';
+import Pagination from './components/Pagination/Pagination';
 function App() {
   return (
     <div className="App">
-      <MyButton>Submit</MyButton>
-      <MyButton variant="outlined" color="secondary" size="small">submit</MyButton>
+      {/* <MyButton>Submit</MyButton>
+      <MyButton variant="outlined" color="secondary" size="small">submit</MyButton> */}
+      <Pagination />
     </div>
   );
 }
