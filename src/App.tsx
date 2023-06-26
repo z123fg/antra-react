77903954bf6eaabcb4d20745440700c86af5fab0
Jing060206 <109713103+Jing060206@users.noreply.github.com>
2023-06-25 19:48:13 -0500

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MyButton from "./components/Button/Button"
+import React from "react";
+import "./App.css";
+import MyButton from "./components/Button/Button";
 function App() {
   return (
     <div className="App">
       <MyButton>Submit</MyButton>
-      <MyButton variant="outlined" color="secondary" size="small">submit</MyButton>
+      <MyButton variant="outlined" color="secondary" size="small">
+        submit
+      </MyButton>
+      <MyButton size="small" isLoading>
+        Loading
+      </MyButton>
     </div>
   );
 }
