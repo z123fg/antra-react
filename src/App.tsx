b364beaec2b4b9559a2f5aea25561ef194f5dae4
zackfei0721 <zackfei0721@gmail.com>
2023-06-27 22:36:03 -0400

    Added Accordion component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Accordion from './components/Accordion/Accordion';
 import MyButton from "./components/Button/Button"
 function App() {
   return (
     <div className="App">
       <MyButton>Submit</MyButton>
       <MyButton variant="outlined" color="secondary" size="small">submit</MyButton>
+      <Accordion
+        summary="What is React?"
+        details="React is a front end JavaScript framework"
+        expandIcon="+"
+      />
     </div>
   );
 }
