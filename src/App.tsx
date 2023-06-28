44718c6206a78a0b8eb3a310bd465705cfc55130
Yifei <923120244a@gmail.com>
2023-06-28 13:43:38 -0400

    add slider example to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyButton from "./components/Button/Button"
+import Slider from './components/Slider/Slider';
 function App() {
   return (
     <div className="App">
       <MyButton>Submit</MyButton>
       <MyButton variant="outlined" color="secondary" size="small">submit</MyButton>
+      <Slider size={'big'} rangeSelector={true} color={'pink'} min={0} max={1000} step={10} mark={30} />
     </div>
   );
 }
